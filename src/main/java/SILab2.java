b9b1527630e9ee85dbf55495a041d2e02df0348c
Argetim Ramadani <argdev@outlook.com>
2020-06-14 00:20:20 +0200

    Update SILab2.java

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -26,36 +26,35 @@ class User {
 
 public class SILab2 {
 
-    public boolean function (User user, List<String> allUsers) {
-        String specialCharacters="!#$%&'()*+,-./:;<=>?@[]^_`{|}";
-        if (user==null) { // A
-            throw new RuntimeException("The user is not instantiated"); // B
+    public boolean function(User user, List<String> allUsers) {
+        String specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}";
+        if (user == null) { // 1
+            throw new RuntimeException("The user is not instantiated"); // 2
         }
-        if (user.getUsername()==null || user.getPassword()==null) // C
-            throw new RuntimeException("The user is missing some mandatory information"); // D
+        if (user.getUsername() == null || user.getPassword() == null) // 3
+            throw new RuntimeException("The user is missing some mandatory information"); // 4
         String password = user.getPassword();
         String passwordLower = password.toLowerCase();
-        if (passwordLower.contains(user.getUsername().toLowerCase())) { // E
-            return false; // F
-        }
-        else if (passwordLower.length()<8) // G
-            return false; // H
+        if (passwordLower.contains(user.getUsername().toLowerCase())) { // 5
+            return false; // 6
+        } else if (passwordLower.length() < 8) // 7
+            return false; // 8
         else {
             boolean digit = false, upper = false, special = false;
 
-            for (int i=0; // I
-                 i<password.length(); // J
-                 i++) { // K
-                if (Character.isDigit(password.charAt(i))) // L
-                    digit = true; // M
-                if (Character.isUpperCase(password.charAt(i))) // N
-                    upper = true; // O
-                if (specialCharacters.contains(String.valueOf(password.charAt(i)))) // P
-                    special = true; // Q
-            }
-            if (!digit || !upper || !special) // R
-                return false; // S
+            for (int i = 0; // 9.1
+                 i < password.length(); // 9.2
+                 i++) { // 9.3
+                if (Character.isDigit(password.charAt(i))) // 10
+                    digit = true; // 11
+                if (Character.isUpperCase(password.charAt(i))) // 12
+                    upper = true; // 13
+                if (specialCharacters.contains(String.valueOf(password.charAt(i)))) // 14
+                    special = true; // 15
+            } // 16
+            if (!digit || !upper || !special) // 17
+                return false; // 18
         }
-        return true; // T
-    }
+        return true; // 19
+    } // 20
 }
